491e7982c1db19362d3d7f9a3333b51cf6ef439b
Cristian Gutierrez <cristiangutierrezcruz67@gmail.com>
2025-08-20 23:56:14 -0300

    working.5

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router";
-import Navbar from "./components/Navbar";
 
 import Creation from "./pages/Creation.tsx";
 
